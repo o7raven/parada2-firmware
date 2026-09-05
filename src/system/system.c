0daa012908c0ff96095003cc116d63bd699bb265
Jakub Farnik <yakubiq@proton.me>
2026-09-05 13:43:55 +0200

    Error handling

--- src/system/system.c
+++ src/system/system.c
@@ -1,10 +1,32 @@
 #include "system.h"
 
-
 status_t system_init(void){
-    blink_init();
+    status_t init_status = STATUS_OK;
+
+    logging_init();
+    log_info("Logging initialization successful");
+    log_info("Starting system initialization ...");
+
+    log_info("Info log working");
+    log_warning("Warning log working");
+    log_error("Error log working", STATUS_DUMMY_ERROR);
+
+    init_status = blink_init();
+    if(init_status != STATUS_OK){
+        log_error("Blink initialization failed", init_status);
+        return STATUS_ERROR;
+    }
+    log_info("Blink initialization successful");
     blink(300, 3);
-    communication_init();
+
+    init_status = communication_init();
+    if(init_status != STATUS_OK){
+        log_error("Communication initialization failed", init_status);
+        return STATUS_ERROR;
+    }
+    log_info("Communication initialization successful");
+
+
 
     return STATUS_OK;
 }
