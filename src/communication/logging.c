c396ae1356f96d0f1457ad06c50296968d33805f
Jakub Farnik <yakubiq@proton.me>
2026-09-05 21:43:03 +0200

    Fixed formatting issue

--- src/communication/logging.c
+++ src/communication/logging.c
@@ -6,15 +6,15 @@ status_t logging_init(void){
 }
 
 void log_info(const char* message){
-    printf("[\\033[34mINFO\\033[0m]: %s\n", message);
+    printf("[\033[34mINFO\033[0m]: %s\n", message);
 }
 void log_warning(const char* message){
-    printf("[\\033[33mWARNING\\033[0m]: %s\n", message);
+    printf("[\033[33mWARNING\033[0m]: %s\n", message);
 }
 void log_error(const char* message, status_t status){
-    printf("[\\033[31mERROR\\033[0m]: %s (Status: %d)\n", message, status);
+    printf("[\033[31mERROR\033[0m]: %s (Status: %d)\n", message, status);
 }
 
 void log_success(const char* message){
-    printf("[\\033[32mSUCCESS\\033[0m]: %s\n", message);
+    printf("[\033[32mSUCCESS\033[0m]: %s\n", message);
 }
