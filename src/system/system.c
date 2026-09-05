0346299929a66fd6c09bc8380f4f9d6fa01821d6
Jakub Farnik <yakubiq@proton.me>
2026-09-05 21:47:32 +0200

    state handler

--- src/system/system.c
+++ src/system/system.c
@@ -43,8 +43,26 @@ status_t system_init(void){
 void system_run(void){
     log_info("Starting system run loop ...");
     while(1){
-        // ... 
+        switch(system_state_machine.current_state){
+        case STATE_CHECK:
+                // Perform checks and update system context
+                break;
+            case STATE_RUN:
+                // Perform normal operations
+                break;
+            case STATE_SAFE:
+                // Perform safe mode operations (Prob just less frequent data collection and telemtry)
+                break;
+            case STATE_ERROR:
+                // Handle error conditions
+                break;
+            default:
+                log_error("Unknown state encountered", STATUS_ERROR);
+                break;
+        }
+        // Update system context here based on sensor readings, communication status, etc.
+
         state_machine_step(&system_state_machine, &system_ctx);
-        // ...
+
     }
 }
