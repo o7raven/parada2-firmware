1e1600565e29cf96285f2dfc079488b57b7f3574
Jakub Farnik <yakubiq@proton.me>
2026-09-05 14:35:15 +0200

    State machine design change

--- src/system/state_machine.h
+++ src/system/state_machine.h
@@ -1,16 +1,22 @@
 #ifndef STATE_MACHINE__H
 #define STATE_MACHINE__H
 
+#include "system/status.h"
+
 typedef enum{
     STATE_INIT,
     STATE_CHECK,
     STATE_RUN,
-    STATE_SENSOR,
-    STATE_TELEMETRY,
     STATE_SAFE,
     STATE_ERROR,
 } system_state_t;
 
-system_state_t state_machine_step(system_state_t current_state);
+typedef struct{
+    system_state_t current_state;
+    system_state_t previous_state;
+} state_machine_t;
+
+status_t state_machine_init(state_machine_t* state_machine);
+system_state_t state_machine_step(state_machine_t* state_machine);
 
 #endif //STATE_MACHINE__H
