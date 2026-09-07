57606beb4b1850c9fe278b1ddc8229e81361847c
o7raven <jakub@farnik.info>
2026-09-07 15:38:17 +0200

    changed state machine transition logic

--- src/system/state_machine.c
+++ src/system/state_machine.c
@@ -1,4 +1,5 @@
 #include "state_machine.h"
+#include "communication/logging.h"
 
 status_t state_machine_init(state_machine_t *state_machine,
                             system_context_t *ctx) {
@@ -14,59 +15,53 @@ status_t state_machine_init(state_machine_t *state_machine,
   return STATUS_OK;
 }
 
-void state_machine_step(
-    state_machine_t* state_machine,
-    system_context_t* ctx
-) {
+void state_machine_step(state_machine_t *state_machine, system_context_t *ctx) {
 
-    system_state_t next_state = state_machine->current_state;
-    
-    switch (state_machine->current_state) {
-        case STATE_CHECK:
-          state_machine->previous_state = state_machine->current_state;
-          if (ctx->sensors_ok && ctx->radio_ok &&
-              ctx->power_ok) {
-            next_state = STATE_RUN;
-          } else {
-            next_state = STATE_SAFE;
-          }
-            break;
+  system_state_t next_state = state_machine->current_state;
 
-        case STATE_RUN:
-          state_machine->previous_state = state_machine->current_state;
-          if(ctx->critical_fault){
-            next_state = STATE_ERROR;
-          }
-          if(ctx->low_battery){
-            next_state = STATE_SAFE;
-          }
-          break;
+  switch (state_machine->current_state) {
+  case STATE_CHECK:
+    if ((ctx->sensors_ok && ctx->radio_ok && ctx->power_ok) &&
+        ctx->low_battery == false
+    ) {
+      next_state = STATE_RUN;
+    } else {
+      next_state = STATE_SAFE;
+    }
+    break;
 
-        case STATE_SAFE:
-            if(ctx->critical_fault){
-                next_state = STATE_ERROR;
-            }
-            if(ctx->sensors_ok && ctx->radio_ok && ctx->power_ok){
-                next_state = STATE_RUN;
-            }
-            if(!ctx->low_battery && ctx->power_ok){
-                next_state = STATE_RUN;
-            }
-            break;
+  case STATE_RUN:
+    if (ctx->critical_fault) {
+      next_state = STATE_ERROR;
+    }
+    if (ctx->low_battery) {
+      next_state = STATE_SAFE;
+    }
+    break;
 
-        case STATE_ERROR:
-            // Handle error conditions
-            // Transition back to INIT or SAFE as needed
-            break;
+  case STATE_SAFE:
+    if (ctx->critical_fault) {
+      next_state = STATE_ERROR;
+    }
+    if (!ctx->low_battery) {
+      next_state = STATE_RUN;
+    }
+    break;
 
-        default:
-            next_state = STATE_ERROR; // Unknown state, transition to ERROR
-            break;
+  case STATE_ERROR:
+    // Handle error conditions
+    // Transition back to INIT or SAFE as needed
+    // now for watchdog
+      break;
 
-        }
-        if (next_state != state_machine->current_state) {
-          state_machine->previous_state = state_machine->current_state;
-          state_machine->current_state = next_state;
-          state_machine->state_time_ms = 0;
-        }
+  default:
+    next_state = STATE_ERROR; // Unknown state, transition to ERROR
+    break;
+  }
+  if (next_state != state_machine->current_state) {
+    log_info("Changing states from %d to %d after %dms", state_machine->current_state, next_state, state_machine->state_time_ms);
+    state_machine->previous_state = state_machine->current_state;
+    state_machine->current_state = next_state;
+    state_machine->state_time_ms = 0;
+  }
 }
