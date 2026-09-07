933e0edcec6a7652c91d7f8b86f75e61e31cd368
o7raven <jakub@farnik.info>
2026-09-07 15:59:33 +0200

    basic func structure

--- src/sensors/bme280.c
+++ src/sensors/bme280.c
@@ -1,1 +1,10 @@
 #include "bme280.h"
+#include "system/status.h"
+
+status_t configure_bme280(bme280_t* bme280_handler){
+    return STATUS_ERROR;
+}
+
+status_t read_bme(bme280_t* bme280_handler){
+    return STATUS_ERROR;
+}
