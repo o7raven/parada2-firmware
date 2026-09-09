d5ac4a5382c0ad3a0ac09316f5ac4eab4e8e1ef4
o7raven <jakub@farnik.info>
2026-09-09 13:10:23 +0200

    added missing sensors

--- src/sensors/sensors.c
+++ src/sensors/sensors.c
@@ -1,16 +1,36 @@
 #include "sensors.h"
+#include "drivers/IMU/imu.h"
+#include "drivers/bme_280/bme280.h"
+#include "drivers/hmc5883l/hmc5883l.h"
 #include "system/state_machine.h"
+#include "system/status.h"
 
 status_t init_sensors(sensors_t* sens_handler, system_context_t* ctx){
     ctx->sensors_ok = false;
-    configure_bme280(
-        &(sens_handler->bme280));
-    // configure other sensors
-    return STATUS_NOT_IMPLEMENTED;
+
+    if(configure_bme280(&(sens_handler->bme280)) != STATUS_OK){
+        return STATUS_SENSOR_ERROR_BME_CONFIG;
+    }
+    if(configure_hmc(&(sens_handler->hmc))!=STATUS_OK){
+        return STATUS_SENSOR_ERROR_HMC_CONFIG;
+    }
+    if(configure_imu(&(sens_handler->imu))!=STATUS_OK){
+        return STATUS_SENSOR_ERROR_IMU_CONFIG;
+    }
+    return STATUS_OK;
 }
 
 status_t read_sensors(sensors_t* sens_handler, system_context_t* ctx){
-    return STATUS_NOT_IMPLEMENTED;
+    if(read_bme(&sens_handler->bme280) != STATUS_OK){
+        return STATUS_SENSOR_ERROR_BME_READ;
+    }
+    if(read_hmc(&sens_handler->hmc) != STATUS_OK){
+        return STATUS_SENSOR_ERROR_HMC_READ;
+    }
+    if(read_imu(&sens_handler->imu) != STATUS_OK){
+        return STATUS_SENSOR_ERROR_IMU_READ;
+    }
+    return STATUS_OK;
 }
 
 status_t sensors_check_health(system_context_t* ctx){
--- src/sensors/sensors.h
+++ src/sensors/sensors.h
@@ -4,9 +4,13 @@
 #include "drivers/bme_280/bme280.h"
 #include "system/status.h"
 #include "system/state_machine.h"
+#include "drivers/hmc5883l/hmc5883l.h"
+#include "drivers/IMU/imu.h"
 
 typedef struct {
    bme280_t bme280; 
+   hmc5883l_t hmc;
+   imu_t imu;
 } sensors_t;
 
 status_t init_sensors(sensors_t* sens_handler, system_context_t* ctx);
