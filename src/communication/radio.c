7e623d7733f4ef659774d85981c42beb53cfb215
o7raven <jakub@farnik.info>
2026-09-09 09:39:00 +0200

    radio definition changed

--- src/communication/radio.c
+++ src/communication/radio.c
@@ -3,6 +3,8 @@
 
 packet_t create_packet(void){
 }
-status_t data_send(void){
+
+
+status_t data_send(sensors_t* _s_handler, gps_t* _gps_handler){
     return STATUS_NOT_IMPLEMENTED;
 }
--- src/communication/radio.h
+++ src/communication/radio.h
@@ -2,6 +2,8 @@
 
 #include "system/status.h"
 #include "sensors/sensors.h"
+#include "drivers/GPS/gps.h"
+#include "sensors/sensors.h"
 
 
 typedef struct{
@@ -9,7 +11,7 @@ typedef struct{
 } packet_t;
 
 packet_t create_packet(void);
-status_t data_send(void);
+status_t data_send(sensors_t* _s_handler, gps_t* _gps_handler);
 
 #define _RAIDIO__H_
 #endif // _RADIO__H_
