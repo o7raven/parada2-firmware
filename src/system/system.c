b58ee43111ed274f4c6a100ba0f1f1b1046e3318
o7raven <jakub@farnik.info>
2026-09-10 13:06:24 +0200

    radio callback and logic change

--- src/system/system.c
+++ src/system/system.c
@@ -1,8 +1,15 @@
 #include "system.h"
+#include "communication/radio.h"
+#include "config/system_config.h"
+#include "system/state_machine.h"
+#include <pico/time.h>
 
 // TODO: Implement a custom error status for each error type
 status_t system_init(void) {
   status_t init_status = STATUS_OK;
+
+  radio_is_due = false;
+  radio_callback_is_running = false;
 
   logging_init();
   log_info("Info log working");
@@ -40,7 +47,7 @@ status_t system_init(void) {
   log_success("State machine initialization successful");
 
   // @Watchdog
-  watchdog_enable(WATCHDOG_TIMEOUT_MS, WATCHDOG_PAUSE_ON_DBG);
+  watchdog_enable(WATCHDOG_TIMEOUT_ms+DATA_COLLECTION_PERIOD_ms, WATCHDOG_PAUSE_ON_DBG);
 
   log_success("Initialization complete");
   log_info("State machine starting ... ");
@@ -48,49 +55,62 @@ status_t system_init(void) {
   return STATUS_OK;
 }
 
+bool radio_callback(struct repeating_timer *t) {
+  radio_is_due = true;
+  return true;
+}
+void create_radio_timer(system_state_t state) {
+  if (radio_callback_is_running) {
+    cancel_repeating_timer(&radio_timer);
+  }
+  switch (state) {
+  case STATE_RUN:
+    add_repeating_timer_ms(RUN_STATE_PACKET_FREQUENCY_ms, &radio_callback,
+                           false, &radio_timer);
+    break;
+  case STATE_SAFE:
+    add_repeating_timer_ms(SAFE_STATE_PACKET_FREQUENCY_ms, &radio_callback,
+                           false, &radio_timer);
+    break;
+  case STATE_ERROR:
+    cancel_repeating_timer(&radio_timer);
+    radio_callback_is_running = false;
+    break;
+  default:
+    break;
+  }
+}
+
 void system_run(void) {
   log_info("Starting system run loop ...");
   while (1) {
+
+    if(state_machine_changed()){
+      create_radio_timer(system_state_machine.current_state);
+    }
+
     // @Note create a task with instead of log every loop
     log_info("In [%d] for %d ms", system_state_machine.current_state,
              system_state_machine.state_time_ms);
-    // Update system context here based on sensor readings, communication
-    // status, etc. communication, power, sensors with ctx
 
     // @Note : state_machine_step() only makes decisions about states
     state_machine_step(&system_state_machine, &system_ctx);
+    system_work();
 
-    switch (system_state_machine.current_state) {
-
-    // @Note : each func updates its own context -> removes the need for a
-    // global update_context function
-    case STATE_RUN:
-      // DO EVERY X SECONDS
-      read_sensors(&sensors, &system_ctx);
-      get_location(&gps_data, &system_ctx);
-      data_send(&sensors, &gps_data, &system_ctx);
-      break;
-
-    case STATE_SAFE:
-      // DO EVERY Y SECONDS
-
-      read_sensors(&sensors, &system_ctx);
-      get_location(&gps_data, &system_ctx);
-      data_send(&sensors, &gps_data, &system_ctx);
-      break;
-
-    case STATE_ERROR:
-      if (system_state_machine.state_time_ms > 30 * 1000) {
-        watchdog_reboot(0, 0, 500);
-      }
-      break;
-
-    default:
-      log_error("Unknown state encountered", STATUS_ERROR);
-      break;
+    if(radio_is_due){
+      radio_is_due = false;
+      send_data(&sensors, &gps_data, &system_ctx);
     }
 
     // @Watchdog
+    sleep_ms(DATA_COLLECTION_PERIOD_ms);
+
     watchdog_update();
   }
+}
+
+
+void system_work(void){
+      read_sensors(&sensors, &system_ctx);
+      get_location(&gps_data, &system_ctx);
 }
--- src/system/system.h
+++ src/system/system.h
@@ -13,11 +13,16 @@
 #include "drivers/GPS/gps.h"
 
 #include "hardware/watchdog.h"
+#include <pico/time.h>
 
 static state_machine_t system_state_machine;
 static system_context_t system_ctx;
 static sensors_t sensors;
 static gps_t gps_data;
+
+static volatile bool radio_is_due;
+static repeating_timer_t radio_timer;
+static bool radio_callback_is_running;
 
 /* @brief Initializes the system and its components
  * @return STATUS_OK if initialization was successful, otherwise returns an error status
@@ -25,8 +30,16 @@ static gps_t gps_data;
  *       called before any other
  *       system functions are used and should only be called once.
  */
+
+
 status_t system_init(void);
 
+void system_work(void);
+
 void system_run(void);
+
+void create_radio_timer(system_state_t state);
+
+bool radio_callback(struct repeating_timer *t);
 
 #endif //SYSTEM__H
