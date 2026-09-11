66b829cf22ac6e6cd1331a0addb2e9a8b8843b19
o7raven <jakub@farnik.info>
2026-09-11 12:34:50 +0200

    log instead of return

--- src/sensors/sensors.c
+++ src/sensors/sensors.c
@@ -1,4 +1,5 @@
 #include "sensors.h"
+#include "communication/logging.h"
 #include "drivers/IMU/imu.h"
 #include "drivers/bme_280/bme280.h"
 #include "drivers/hmc5883l/hmc5883l.h"
@@ -9,26 +10,27 @@ status_t init_sensors(sensors_t* sens_handler, system_context_t* ctx){
     ctx->sensors_ok = false;
 
     if(configure_bme280(&(sens_handler->bme280)) != STATUS_OK){
-        return STATUS_SENSOR_ERROR_BME_CONFIG;
+        log_error("Config error", STATUS_SENSOR_ERROR_BME_CONFIG);
     }
     if(configure_hmc(&(sens_handler->hmc))!=STATUS_OK){
-        return STATUS_SENSOR_ERROR_HMC_CONFIG;
+        log_error("Config error",STATUS_SENSOR_ERROR_HMC_CONFIG);
     }
     if(configure_imu(&(sens_handler->imu))!=STATUS_OK){
-        return STATUS_SENSOR_ERROR_IMU_CONFIG;
+        log_error("Config error",STATUS_SENSOR_ERROR_IMU_CONFIG);
     }
     return STATUS_OK;
 }
 
 status_t read_sensors(sensors_t* sens_handler, system_context_t* ctx){
     if(read_bme(&sens_handler->bme280) != STATUS_OK){
-        return STATUS_SENSOR_ERROR_BME_READ;
+        log_error("Read error", STATUS_SENSOR_ERROR_BME_READ);
     }
     if(read_hmc(&sens_handler->hmc) != STATUS_OK){
-        return STATUS_SENSOR_ERROR_HMC_READ;
+        log_error("Read error",STATUS_SENSOR_ERROR_HMC_READ);
     }
     if(read_imu(&sens_handler->imu) != STATUS_OK){
-        return STATUS_SENSOR_ERROR_IMU_READ;
+        log_error("Read error",STATUS_SENSOR_ERROR_IMU_READ);
+
     }
     return STATUS_OK;
 }
