58780ecf33d96d9eaae0b645d0f3a67e431edbe3
Jakub Farnik <yakubiq@proton.me>
2026-09-14 11:39:39 +0200

    fixed blink timer blocking radio

--- src/misc/blink.c
+++ src/misc/blink.c
@@ -1,17 +1,26 @@
 #include "blink.h"
 
 static uint8_t blink_remaining = 0;
+static repeating_timer_t blink_timer;
+static bool blink_timer_running = false;
 
-status_t blink_init(void){
-    gpio_init(PIN_LED);
-    gpio_set_dir(PIN_LED, GPIO_OUT);
-    gpio_put(PIN_LED, 0);
-    return STATUS_OK;
+status_t blink_init(void) {
+  gpio_init(PIN_LED);
+  gpio_set_dir(PIN_LED, GPIO_OUT);
+  gpio_put(PIN_LED, 0);
+
+  blink_timer_running = false;
+
+  return STATUS_OK;
 }
 
 void blink(uint16_t duration, uint8_t count){
+    if(blink_timer_running){
+        cancel_repeating_timer(&blink_timer);
+        blink_timer_running = false;
+    }
    blink_remaining = count * 2;
-   add_repeating_timer_ms(
+   blink_timer_running = add_repeating_timer_ms(
     duration,
     blink_callback,
     NULL,
@@ -24,8 +33,8 @@ bool blink_callback(repeating_timer_t *rt){
         gpio_xor_mask(1 << PIN_LED);
         blink_remaining--;
         return true;
-    } else {
-        gpio_put(PIN_LED, 0);
-        return false;
     }
+    blink_timer_running = false;
+    gpio_put(PIN_LED, 0);
+    return false;
 }
--- src/misc/blink.h
+++ src/misc/blink.h
@@ -5,8 +5,6 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 
-static repeating_timer_t blink_timer;
-static uint8_t blink_remaining;
 
 status_t blink_init(void);
 void blink(uint16_t duration, uint8_t count);
