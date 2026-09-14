11155e1b41788c9d2ed07068ef6ef8de0be0cd6d
Jakub Farnik <yakubiq@proton.me>
2026-09-14 11:39:45 +0200

    logic fix

--- src/system/state_machine.c
+++ src/system/state_machine.c
@@ -14,6 +14,7 @@ status_t state_machine_init(state_machine_t *state_machine) {
   return STATUS_OK;
 }
 
+
 void state_machine_step(state_machine_t *state_machine, system_context_t *ctx) {
 
   system_state_t next_state = state_machine->current_state;
@@ -23,7 +24,7 @@ void state_machine_step(state_machine_t *state_machine, system_context_t *ctx) {
   case STATE_RUN:
     if (ctx->critical_fault) {
       next_state = STATE_ERROR;
-    }else if(ctx->low_battery || ctx->power_ok) {
+    }else if(ctx->low_battery || !ctx->power_ok) {
       next_state = STATE_SAFE;
     }
     break;
