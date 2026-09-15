0d6ba852ccbb1dc0a3dda5fa8820f0b5579ed0ac
Jakub Farnik <yakubiq@proton.me>
2026-09-15 11:13:48 +0200

    function pointer and scan implementation

--- src/drivers/bme_280/bme280_driver.c
+++ src/drivers/bme_280/bme280_driver.c
@@ -1,17 +1,77 @@
 #include "bme280_driver.h"
+#include "communication/logging.h"
+#include "drivers/i2c/i2c.h"
+#include "libs/bosch/bme280.h"
+#include "libs/bosch/bme280_defs.h"
 #include "system/status.h"
+#include <pico/time.h>
+#include <stdint.h>
 
 
 
+// @Note add proper error handling
+void configure_bme280(){
+    struct bme280_settings settings;
+    log_info("Configuring bme280 ...");
+    
+    dev.read = bme280_i2c_read;
+    dev.write = bme280_i2c_write;
 
-status_t configure_bme280(bme280_t* bme280_handler){
-    dev.intf = BME280_I2C_INTF;
+    uint8_t dev_addr = BME280_I2C_INTF;
+    dev.intf = dev_addr;
+    dev.intf_ptr = &dev_addr;
+
+    //sleep before config
+    sleep_ms(100);
+
+    dev.delay_us = bme280_delay_us;
+
+    settings.filter = BME280_FILTER_COEFF_2;
+    settings.osr_h = BME280_OVERSAMPLING_1X;
+    settings.osr_p = BME280_OVERSAMPLING_1X;
+    settings.osr_t = BME280_OVERSAMPLING_1X;
+
+    settings.standby_time = BME280_STANDBY_TIME_0_5_MS;
+
+    bme280_set_sensor_settings(BME280_SEL_ALL_SETTINGS, &settings, &dev);
+
+    bme280_set_sensor_mode(BME280_POWERMODE_NORMAL, &dev);
+    bme280_cal_meas_delay(&period, &settings);
 
     bme280_init(&dev);
+    log_success("BME280 configured successfully");
+    
+}
 
+status_t read_bme280(struct bme280_data* bme280_handler){
+    log_info("Reading bme280 ...");
     return STATUS_NOT_IMPLEMENTED;
 }
 
-status_t read_bme(bme280_t* bme280_handler){
-    return STATUS_NOT_IMPLEMENTED;
+void bme280_delay_us(uint32_t period, void *intf_ptr){
+    uint32_t period_ms = period/1000;
+    sleep_ms(period_ms);
+}
+BME280_INTF_RET_TYPE bme280_i2c_write(uint8_t reg_addr, const uint8_t *reg_data,
+                                      uint32_t length, void *intf_ptr) {
+  abstract_i2c_write(reg_addr, reg_data, length, *(uint8_t *)intf_ptr);
+  return BME280_INTF_RET_SUCCESS;
+}
+
+BME280_INTF_RET_TYPE bme280_i2c_read(uint8_t reg_addr, uint8_t *reg_data,
+                                     uint32_t length, void *intf_ptr) {
+  abstract_i2c_read(reg_addr, reg_data, length, *(uint8_t *)intf_ptr);
+  return BME280_INTF_RET_SUCCESS;
+}
+
+void bme280_scan(struct bme280_data* comp_data){
+    int8_t idx = 0;
+    uint8_t status_reg;
+
+    bme280_get_regs(BME280_REG_STATUS, &status_reg, 1, &dev);
+    if(status_reg & BME280_STATUS_MEAS_DONE){
+        dev.delay_us(period, dev.intf_ptr);
+        bme280_get_sensor_data(BME280_TEMP | BME280_HUM | BME280_PRESS,
+                               comp_data, &dev);
+    }
 }
--- src/drivers/bme_280/bme280_driver.h
+++ src/drivers/bme_280/bme280_driver.h
@@ -1,14 +1,16 @@
 /*
 
         * Driver written by Jakub Farnik 2026
-        * Reference datasheet https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
+        * Reference datasheet
+   https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
 
 */
 #ifndef _BME280__H
 #define _BME280__H
 
-#include "system/status.h"
+#include "drivers/i2c/i2c.h"
 #include "libs/bosch/bme280.h"
+#include "system/status.h"
 
 /* Difference between BME280 and BMP280
 5.2 Register compatibility to BMP280
@@ -20,7 +22,7 @@ chip_id                 0x60                    0x56-58
 
 0xF7...0xF9 "press"
     BMP280: Resolution (16...20bit) depends only on osrs_p
-    BME280: Without filter, resolution depends on osrs_p; 
+    BME280: Without filter, resolution depends on osrs_p;
             when using filter, resolution is always 20bit
 0xFA...0xFC "temp"
     BMP280: Resolution (16...20bit) only depends on osrs_t
@@ -31,7 +33,7 @@ chip_id                 0x60                    0x56-58
 /* Register definitions */
 #define REG_CHIP_ID _u(0xD0)
 #define REG_RESET _u(0xE0)
-    #define REG_STATUS _u(0xF3) // 2 bits measuring(0) and im_update(3y)
+#define REG_STATUS _u(0xF3) // 2 bits measuring(0) and im_update(3y)
 
 #define REG_CTRL_HUM _u(0xF2)
 #define REG_CTRL_MEAS _u(0xF4)
@@ -67,26 +69,30 @@ chip_id                 0x60                    0x56-58
 */
 
 typedef enum {
-    SENSOR_OK=0,
-    COM_ERR_WRONG_DEV=10,
-    TRIM_DATA_OOB=20,
-    TEMP_BOND_WIRE_FAIL_MEMS_D=30,
-    PRESS_BOND_WIRE_FAIL_MEMS_D=31,
-    IMPLAUSIBLE_TEMP=40,
-    IMPLAUSIBLE_PRESS=41,
-    IMPLAUSIBLE_HUM=42
+  SENSOR_OK = 0,
+  COM_ERR_WRONG_DEV = 10,
+  TRIM_DATA_OOB = 20,
+  TEMP_BOND_WIRE_FAIL_MEMS_D = 30,
+  PRESS_BOND_WIRE_FAIL_MEMS_D = 31,
+  IMPLAUSIBLE_TEMP = 40,
+  IMPLAUSIBLE_PRESS = 41,
+  IMPLAUSIBLE_HUM = 42
 } bme280_error_t;
 
-typedef struct{
-    float pressure;
-    float humidity;
-    float temperature;
-} bme280_t;
+
 
 struct bme280_dev dev;
+static uint32_t period;
 
-status_t configure_bme280(bme280_t* bme280_handler);
-status_t read_bme(bme280_t* bme280_handler);
+void configure_bme280();
 
+void bme280_delay_us(uint32_t period, void *intf_ptr);
 
+// I2C functions port to Pico SDK
+BME280_INTF_RET_TYPE bme280_i2c_write(uint8_t reg_addr, const uint8_t *reg_data,
+                                      uint32_t length, void *intf_ptr);
+BME280_INTF_RET_TYPE bme280_i2c_read(uint8_t reg_addr, uint8_t *reg_data,
+                                     uint32_t length, void *intf_ptr);
+
+void bme280_scan(struct bme280_data* comp_data);
 #endif // _BME280__H
