55f88fda86c88715d9f5094da41d40d91a0555f9
Jakub Farnik <yakubiq@proton.me>
2026-09-15 16:47:58 +0200

    fixed wrong com

--- src/drivers/i2c/i2c.c
+++ src/drivers/i2c/i2c.c
@@ -1,4 +1,5 @@
 #include "i2c.h"
+#include "communication/logging.h"
 #include "system/status.h"
 #include <hardware/i2c.h>
 #include <stdbool.h>
@@ -17,17 +18,21 @@ status_t abstract_i2c_init(void) {
 
 status_t abstract_i2c_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t length, uint8_t dev_addr){
     uint8_t buffer_to_write[length+1];
-    buffer_to_write[0] = dev_addr;
+    buffer_to_write[0] = reg_addr;
     for(uint32_t i = 0; i < length; i++){
         buffer_to_write[i+1] = reg_data[i];
     }
-    i2c_write_blocking(I2C_PORT, dev_addr, buffer_to_write, length, true);
-    return STATUS_NOT_IMPLEMENTED;
+    int result = i2c_write_blocking(I2C_PORT, dev_addr, buffer_to_write, length+1, false);
+    if(result <0){
+        log_error("Error in i2c write", STATUS_ERROR);
+        return STATUS_ERROR;
+    }
+    return STATUS_OK;
 }
 
 status_t abstract_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t length,
                            uint8_t dev_addr) {
   i2c_write_blocking(I2C_PORT, dev_addr, &reg_addr, 1, true);
-  i2c_read_blocking(I2C_PORT, reg_addr, reg_data, length, false);
-  return STATUS_NOT_IMPLEMENTED;
+  i2c_read_blocking(I2C_PORT, dev_addr, reg_data, length + 1, false);
+  return STATUS_OK;
 }
