2003b7529271e5bed8edb3a4f0f9175d2c5b5d1c
Jakub Farnik <yakubiq@proton.me>
2026-09-15 13:21:01 +0200

    i2c pin update

--- src/config/hardware_config.h
+++ src/config/hardware_config.h
@@ -13,11 +13,9 @@
 #define UART_RX_PIN 1
 
 /* I2C*/
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_SDA 26
+#define I2C_SCL 27
 #define I2C_BAUDRATE 400000
 
 /* SPI */
