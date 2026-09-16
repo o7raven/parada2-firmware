1b2b783e2446f4136a6c6a0a524b6ec2437d1f38
Jakub Farnik <yakubiq@proton.me>
2026-09-16 10:00:02 +0200

    context update

--- src/sensors/sensors.c
+++ src/sensors/sensors.c
@@ -8,25 +8,42 @@
 #include "config/system_config.h"
 
 status_t init_sensors(sensors_t* sens_handler, system_context_t* ctx){
-    ctx->sensors_ok = false;
+    ctx->sensors_ok = true;
 
+    status_t init_status = STATUS_OK;
     if(configure_bme280() != STATUS_OK){
         log_error("Config error", STATUS_SENSOR_ERROR_BME_CONFIG);
+        init_status = STATUS_SENSOR_INIT_FAIL;
+
+        ctx->sensors_ok = false;
     }
 
     if(configure_hmc(&(sens_handler->hmc))!=STATUS_OK){
         log_error("Config error",STATUS_SENSOR_ERROR_HMC_CONFIG);
+        init_status = STATUS_SENSOR_INIT_FAIL;
+
+        ctx->sensors_ok = false;
     }
 
     if(configure_imu(&(sens_handler->imu))!=STATUS_OK){
         log_error("Config error",STATUS_SENSOR_ERROR_IMU_CONFIG);
+        init_status = STATUS_SENSOR_INIT_FAIL;
+
+        ctx->sensors_ok = false;
     }
-    return STATUS_OK;
+    return init_status;
 }
 
 status_t read_sensors(sensors_t* sens_handler, system_context_t* ctx){
     log_info("Reaing BME280 ...");
+
+    if(sens_handler == NULL){
+        ctx->sensors_ok = false;
+        log_error("Sens handler is null", STATUS_I2C_ERROR);
+    }
+
     if(bme280_scan(&sens_handler->bme280) != STATUS_OK){
+        ctx->sensors_ok = false;
         log_warning("BME280 Reading faiure...");
     }else {
         log_success("BME280 Reading successfull");
@@ -35,14 +52,13 @@ status_t read_sensors(sensors_t* sens_handler, system_context_t* ctx){
                    sens_handler->bme280.pressure,
                    sens_handler->bme280.humidity);
     }
-    if(sens_handler == NULL){
-        log_error("Sens handler is null", STATUS_I2C_ERROR);
-    }
 
     if(read_hmc(&sens_handler->hmc) != STATUS_OK){
+        ctx->sensors_ok = false;
         log_error("Read error",STATUS_SENSOR_ERROR_HMC_READ);
     }
     if(read_imu(&sens_handler->imu) != STATUS_OK){
+        ctx->sensors_ok = false;
         log_error("Read error",STATUS_SENSOR_ERROR_IMU_READ);
 
     }
