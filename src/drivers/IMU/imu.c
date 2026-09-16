93df273b18e78b817e969e1a4a615ad9e454c96f
Jakub Farnik <yakubiq@proton.me>
2026-09-16 18:21:42 +0200

    i2c com to imu

--- src/drivers/IMU/imu.c
+++ src/drivers/IMU/imu.c
@@ -1,10 +1,55 @@
 #include "imu.h"
+#include "communication/logging.h"
+#include "drivers/i2c/i2c.h"
 #include "system/status.h"
 
 status_t configure_imu(imu_t* imu_handler){
+    uint8_t _chip_Id_buffer[1];
+    abstract_i2c_read(REG_WHO_AM_I, _chip_Id_buffer, 1, mpu_slave_addr);
+    if(*_chip_Id_buffer!=mpu_chip_id){
+        log_warning("The IMU has not been found!");
+        return STATUS_NOT_FOUND;
+    }
+    log_success("IMU has been found"); 
+    uint8_t config_set = 0x00;
+    abstract_i2c_write(REG_PWR_MGMT, &config_set, 1, mpu_slave_addr);
+    abstract_i2c_write(REG_PWR_MGMT, &PLL_X_GYRO, 1, mpu_slave_addr);
 
-    return STATUS_NOT_IMPLEMENTED;
+    abstract_i2c_write(REG_ACCEL_CONFIG, &AFS_SEL, 1, mpu_slave_addr);
+    abstract_i2c_write(REG_GYRO_CONFIG, &FS_SEL, 1, mpu_slave_addr);
+    abstract_i2c_write(REG_CONFIG, &mpu_dlpf_cfg, 1, mpu_slave_addr);
+
+    return STATUS_OK;
 }
 status_t read_imu(imu_t* imu_handler){
-    return STATUS_NOT_IMPLEMENTED;
+  int16_t i16_accX;
+  int16_t i16_accY;
+  int16_t i16_accZ;
+
+  int16_t i16_gyroX;
+  int16_t i16_gyroY;
+  int16_t i16_gyroZ;
+
+  uint8_t acc_data[ACCEL_REG_LEN];
+  abstract_i2c_read(REG_ACCEL_ADDR, acc_data, ACCEL_REG_LEN, mpu_slave_addr);
+  uint8_t gyro_data[GYRO_REG_LEN];
+  abstract_i2c_read(REG_GYRO_ADDR, gyro_data, ACCEL_REG_LEN, mpu_slave_addr);
+
+  i16_accX = (acc_data[0] << 8) | acc_data[1];
+  i16_accY = (acc_data[2] << 8) | acc_data[3];
+  i16_accZ = (acc_data[4] << 8) | acc_data[5];
+
+  imu_handler->acc_X=i16_accX/ACC_LSB_SENSITIVITY;
+  imu_handler->acc_Y=i16_accY/ACC_LSB_SENSITIVITY;
+  imu_handler->acc_Z=i16_accZ/ACC_LSB_SENSITIVITY;
+
+  i16_gyroX = (gyro_data[0] << 8) | gyro_data[1];
+  i16_gyroY = (gyro_data[2] << 8) | gyro_data[3];
+  i16_gyroZ = (gyro_data[4] << 8) | gyro_data[5];
+
+  imu_handler->gyro_X = i16_gyroX / GYRO_LSB_SENSITIVITY;
+  imu_handler->gyro_Y = i16_gyroY / GYRO_LSB_SENSITIVITY;
+  imu_handler->gyro_Z = i16_gyroZ / GYRO_LSB_SENSITIVITY;
+
+  return STATUS_OK;
 }
--- src/drivers/IMU/imu.h
+++ src/drivers/IMU/imu.h
@@ -4,39 +4,40 @@
 #include "system/status.h"
 #include "inttypes.h"
 
-#define mpu_slave_addr = 0x68;
-#define mpu_chip_id = 0x70;
+static uint8_t mpu_slave_addr = 0x68;
+static uint8_t mpu_chip_id = 0x68;
 
-#define REG_WHO_AM_I = 0x75;
-#define REG_PWR_MGMT = 0x6B;
+static uint8_t REG_WHO_AM_I = 0x75;
+static uint8_t REG_PWR_MGMT = 0x6B;
 
 #define G_2 0<<3
 #define G_4 1<<3
 #define G_8 2<<3
 #define G_16 3<<3
-#define AFS_SEL = G_4;
+static uint8_t AFS_SEL = G_4;
 static const float ACC_LSB_SENSITIVITY = 8192.0f;
 
 #define FSR_250 0 << 3
 #define FSR_500 1 << 3
 #define FSR_1000 2 << 3
 #define FSR_2000 3 << 3
-#define FS_SEL = FSR_500;
+static uint8_t FS_SEL = FSR_500;
+
 static const float GYRO_LSB_SENSITIVITY = 65.5f;
-#define PLL_X_GYRO 0x01
+static uint8_t PLL_X_GYRO = 0x01;
 
-#define REG_CONFIG = 0x1a;
-#define mpu_dlpf_cfg = 0x03;
+static uint8_t REG_CONFIG = 0x1a;
+static uint8_t mpu_dlpf_cfg = 0x03;
 
 
-#define REG_ACCEL_CONFIG = 0x1c;
-#define REG_GYRO_CONFIG = 0x1b;
+static uint8_t REG_ACCEL_CONFIG = 0x1c;
+static uint8_t REG_GYRO_CONFIG = 0x1b;
 
-#define REG_ACCEL_ADDR = 0x3B;
-#define ACCEL_REG_LEN = 6;
+static uint8_t REG_ACCEL_ADDR = 0x3B;
+static uint8_t ACCEL_REG_LEN = 6;
 
-#define REG_GYRO_ADDR = 0x43;
-#define GYRO_REG_LEN = 6;
+static uint8_t REG_GYRO_ADDR = 0x43;
+static uint8_t GYRO_REG_LEN = 6;
 
 
 typedef struct{
