7e4b0681316ddadd57015db8ac7087ff4fff201e
Jakub Farnik <yakubiq@proton.me>
2026-09-18 21:15:02 +0200

    hmc id verification

--- src/drivers/hmc5883l/hmc5883l.c
+++ src/drivers/hmc5883l/hmc5883l.c
@@ -1,7 +1,29 @@
 #include "hmc5883l.h"
+#include "communication/logging.h"
+#include "drivers/i2c/i2c.h"
 #include "system/status.h"
 
 status_t configure_hmc(hmc5883l_t* hmc_handler){
+    // Check the Identification registers
+    log_info("Accessing HMC5883l identification registers...");
+    uint8_t sensor_identification[3];
+    if(abstract_i2c_read(REG_IDENTIFICATION_A, &sensor_identification[0], 1, DEV_ADDR) != STATUS_OK){
+        log_warning("There has been an error reading the Identification A register");
+    }
+    if(abstract_i2c_read(REG_IDENTIFICATION_B, &sensor_identification[1], 1, DEV_ADDR) != STATUS_OK){
+        log_warning("There has been an error reading the Identification B register");
+    }
+    if(abstract_i2c_read(REG_IDENTIFICATION_C, &sensor_identification[2], 1, DEV_ADDR) != STATUS_OK){
+        log_warning("There has been an error reading the Identification C register");
+    }
+    if(sensor_identification[0] == ID_A_VAL && sensor_identification[1] == ID_B_VAL && sensor_identification[2] == ID_C_VAL){
+        log_success("HMC5883l has been verified!");
+    }else{
+        log_warning("HMC5883l has not been verified");
+    }
+
+
+
     return STATUS_NOT_IMPLEMENTED;
 }
 status_t read_hmc(hmc5883l_t* hmc_handler){
--- src/drivers/hmc5883l/hmc5883l.h
+++ src/drivers/hmc5883l/hmc5883l.h
@@ -4,32 +4,82 @@
 #include "system/status.h"
 
 /* Register List */
+#define DEV_ADDR (0x1E)
 
-#define REG_CONFIG_A 0x00
-#define REG_CONFIG_B 0x01
-#define REG_MODE 0x02
+#define REG_CONFIG_A (0x00)
+#define REG_CONFIG_B (0x01)
+#define REG_MODE (0x02)
 
-#define REG_DATA_X_OUT_MSB 0x03
-#define REG_DATA_X_OUT_LSB 0x04
+#define REG_DATA_X_OUT_MSB (0x03)
+#define REG_DATA_X_OUT_LSB (0x04)
 
-#define REG_DATA_Z_OUT_MSB 0x05
-#define REG_DATA_Z_OUT_LSB 0x06
+#define REG_DATA_Z_OUT_MSB (0x05)
+#define REG_DATA_Z_OUT_LSB (0x06)
 
-#define REG_DATA_Y_OUT_MSB 0x07
-#define REG_DATA_Y_OUT_LSB 0x08
+#define REG_DATA_Y_OUT_MSB (0x07)
+#define REG_DATA_Y_OUT_LSB (0x08)
 
-#define REG_STATUS 0x09
-#define REG_IDENTIFICATION_A 0x0A
-#define REG_IDENTIFICATION_B 0x0B
-#define REG_IDENTIFICATION_C 0x0C
+#define REG_STATUS (0x09)
 
+#define REG_IDENTIFICATION_A (0x0A)
+#define ID_A_VAL (0b01001000)
+#define REG_IDENTIFICATION_B (0x0B)
+#define ID_B_VAL (0b00110100)
+#define REG_IDENTIFICATION_C (0x0C)
+#define ID_C_VAL (0b00110011)
+
+#define OVERFLOW_VALUE -4096
+
+// Inspired https://github.com/jarzebski/Arduino-HMC5883L/blob/dev/HMC5883L.h
+typedef enum{
+    rate_0p75Hz = 0b000,
+    rate_1p5Hz = 0b001,
+    rate_3Hz = 0b010,
+    rate_7p5Hz = 0b011,
+    rate_15Hz = 0b100,
+    rate_30Hz = 0b101,
+    rate_75Hz = 0b110,
+} output_rate_t;
+
+typedef enum{
+    single = 0b01,
+    continuous = 0b00,
+    idle = 0b10
+} mode_t;
+
+typedef enum{
+    samples_8 = 0b11,
+    samples_4 = 0b10,
+    samples_2 = 0b01,
+    samples_1 = 0b00,
+} samples_t;
+
+typedef enum{
+    range_0p88 = 0b000,
+    range_1p3 = 0b001,
+    range_1p9 = 0b010,
+    range_2p5 = 0b011,
+    range_4 = 0b100,
+    range_4p7 = 0b101,
+    range_5p6 = 0b110,
+    range_8p1 = 0b111
+} gain_settings_t;
 
 typedef struct{
-    
+    float x_axis;
+    float y_axis;
+    float z_axis;
 } hmc5883l_t;
 
 status_t configure_hmc(hmc5883l_t* hmc_handler);
 status_t read_hmc(hmc5883l_t* hmc_handler);
+
+status_t set_meas_mode(mode_t mode);
+status_t set_output_rate(output_rate_t rate);
+status_t set_samples(samples_t samples);
+
+hmc5883l_t self_test();
+
 
 
 #endif //_HMC5883L__H_
