9aaef99c91cacdc7c7fbac308cf768bf67af98e5
Jakub Farnik <yakubiq@proton.me>
2026-09-18 21:15:18 +0200

    definition brackets

--- src/drivers/IMU/imu.h
+++ src/drivers/IMU/imu.h
@@ -10,17 +10,17 @@ static uint8_t mpu_chip_id = 0x68;
 static uint8_t REG_WHO_AM_I = 0x75;
 static uint8_t REG_PWR_MGMT = 0x6B;
 
-#define G_2 0<<3
-#define G_4 1<<3
-#define G_8 2<<3
-#define G_16 3<<3
+#define G_2 (0<<3)
+#define G_4 (1<<3)
+#define G_8 (2<<3)
+#define G_16 (3<<3)
 static uint8_t AFS_SEL = G_4;
 static const float ACC_LSB_SENSITIVITY = 8192.0f;
 
-#define FSR_250 0 << 3
-#define FSR_500 1 << 3
-#define FSR_1000 2 << 3
-#define FSR_2000 3 << 3
+#define FSR_250 (0 << 3)
+#define FSR_500 (1 << 3)
+#define FSR_1000 (2 << 3)
+#define FSR_2000 (3 << 3)
 static uint8_t FS_SEL = FSR_500;
 
 static const float GYRO_LSB_SENSITIVITY = 65.5f;
