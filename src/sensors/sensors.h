8237949544e76bad18520ef7e3c5fd943d82e216
Jakub Farnik <yakubiq@proton.me>
2026-09-22 16:55:35 +0200

    sens context update

--- src/sensors/sensors.h
+++ src/sensors/sensors.h
@@ -4,7 +4,6 @@
 #include "drivers/IMU/imu.h"
 #include "drivers/bme_280/bme280_driver.h"
 #include "system/status.h"
-#include "system/state_machine.h"
 #include "drivers/hmc5883l/hmc5883l.h"
 
 typedef struct {
@@ -13,10 +12,18 @@ typedef struct {
    imu_t imu;
 } sensors_t;
 
-status_t init_sensors(sensors_t* sens_handler, system_context_t* ctx);
+typedef struct{
+   bool bme280_ok;
+   bool hmc5883l_ok;
+   bool imu_ok;
 
-status_t read_sensors(sensors_t* sens_handler, system_context_t* ctx);
+   bool sensors_ok;
+} sensors_context_t;
 
-status_t sensors_check_health(system_context_t* ctx);
+status_t init_sensors(sensors_t* sens_handler, sensors_context_t* ctx);
+
+status_t read_sensors(sensors_t* sens_handler, sensors_context_t* ctx);
+
+status_t sensors_check_health(sensors_context_t* ctx);
 
 #endif // _SENSORS__H
